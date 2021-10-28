8b877c86573c0d13d8373fcbb497630d1cdf7edc
unknown <D:\TheBat_Mail>
2021-10-28 13:06:18 +0300

    Fix some socket connections

--- client/src/store/actions/thunks.ts
+++ client/src/store/actions/thunks.ts
@@ -1,6 +1,6 @@
-import { AppDispatch } from '../index';
+import {AppDispatch} from '../index';
 import socket from '../../socket';
-import { IData, IQuotes } from '../../model';
+import {IData, IQuotes} from '../../model';
 import {
   CreateAllTickersList,
   GetTickersDataAction,
@@ -9,16 +9,24 @@ import {
   SetErrorAction,
   SetIsLoadingAction,
 } from './actions';
-import { AppThunk } from './action.type';
+import {AppThunk} from './action.type';
 
 export const thunkGetData = (): AppThunk => async (dispatch: AppDispatch, getState) => {
   try {
     dispatch(SetIsLoadingAction());
+    await socket.connect();
+    socket.on('connect_error', err => {
+      dispatch(SetErrorAction(`Loading Error: ${err.message}`))
+      setTimeout(() => {
+        socket.connect();
+      }, 1000);
+    })
+
     socket.on('connect', () => {
       console.log('Connected to Server. SocketID:', socket.id, socket.connected);
     });
-    socket.emit('start');
 
+    socket.emit('start');
     socket.once('ticker', (data: IData) => {
       const tickersNames: string[] = data.quotes.map((el: IQuotes) => el.ticker);
       dispatch(GetTickersListAction(tickersNames));
@@ -26,11 +34,14 @@ export const thunkGetData = (): AppThunk => async (dispatch: AppDispatch, getSta
       dispatch(GetTickersDataAction(data));
       dispatch(ResetIsLoadingAction());
     });
-
     socket.on('ticker', (data: IData) => {
       dispatch(GetTickersDataAction(data));
     });
+    socket.on('disconnect', () => {
+      socket.connect();
+    })
+
   } catch (e) {
-    dispatch(SetErrorAction('Loading Error'));
+    dispatch(SetErrorAction(`Loading Error: ${e}`));
   }
 };
